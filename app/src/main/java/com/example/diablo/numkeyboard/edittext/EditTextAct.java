fe0176048f98ea7d9941fed998286844649c9692
Diablo <shenyonghe525@gmail.com>
2017-11-09 16:50:40 +0800

    num key

---
+++ app/src/main/java/com/example/diablo/numkeyboard/edittext/EditTextAct.java
@@ -0,0 +1,19 @@
+package com.example.diablo.numkeyboard.edittext;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.diablo.numkeyboard.R;
+
+/**
+ * Created by Diablo on 2017/11/9.
+ */
+
+public class EditTextAct extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
